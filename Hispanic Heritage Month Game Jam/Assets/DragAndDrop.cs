10f376f80c6894f7af0d69f7ee5bfd4bae9bce66
cmrnprry <perry.ca@husky.neu.edu>
2020-09-20 13:38:34 -0400

    start to the drag and drop and fake title

---
+++ Hispanic Heritage Month Game Jam/Assets/DragAndDrop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private GameObject dragItem;
+
+    //Sets the game Object to be dragged
+    public void SetDragItem(GameObject item)
+    {
+        dragItem = item;
+    }
+
+    //Drags the clicked item
+    public void DragItem()
+    {
+        dragItem.transform.position = Input.mousePosition;
+    }
+}
