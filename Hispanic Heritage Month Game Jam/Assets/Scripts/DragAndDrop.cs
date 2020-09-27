1caebc20627bc847fb13bbcaa10557b0f86cb7ce
cmrnprry <perry.ca@husky.neu.edu>
2020-09-27 13:38:18 -0400

    the hands work :)

--- Hispanic Heritage Month Game Jam/Assets/Scripts/DragAndDrop.cs
+++ Hispanic Heritage Month Game Jam/Assets/Scripts/DragAndDrop.cs
@@ -3,15 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+
+public enum GameState
+{
+    Fold, Filling, Masa
+}
+
 public class DragAndDrop : MonoBehaviour
 {
     private GameObject dragItem;
     public bool isDropZone = false;
+    public Texture2D open, closed;
+    public GameState state = GameState.Masa;
 
-    //Drags the clicked item
-    public void DragItem()
+    private void Update()
     {
-        dragItem.transform.position = Input.mousePosition;
+        switch (state)
+        {
+            case GameState.Masa:
+                break;
+            case GameState.Filling:
+                break;
+            case GameState.Fold:
+                break;
+            default:
+                Debug.LogError("State is wrong");
+                break;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Debug.Log("closed");
+            UpdateHand(closed);
+        }
+        else
+        {
+            Debug.Log("open");
+
+            UpdateHand(open);
+        }
+    }
+
+    private void UpdateHand(Texture2D hand)
+    {
+        CursorMode mode = CursorMode.ForceSoftware;
+        float xspot = hand.width / 4;
+        float yspot = hand.height / 4;
+        Vector2 hotSpot = new Vector2(xspot, yspot);
+        Cursor.SetCursor(hand, hotSpot, mode);
     }
 
     public void DropItem()
@@ -31,6 +70,12 @@ public class DragAndDrop : MonoBehaviour
                     break;
             }
         }
+    }
+
+    //Drags the clicked item
+    public void DragItem()
+    {
+        dragItem.transform.position = Input.mousePosition;
     }
 
     //Sets the game Object to be dragged
