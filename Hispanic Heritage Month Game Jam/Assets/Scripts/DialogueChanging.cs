da395e7b9fd37ab690561f91528950dd978d125e
Samantha Ortiz <samanthaortiz0697@gmail.com>
2020-10-03 21:08:08 -0500

    Fixed dialogue issues cccc;
    can we fix it? YES WE CAN

--- Hispanic Heritage Month Game Jam/Assets/Scripts/DialogueChanging.cs
+++ Hispanic Heritage Month Game Jam/Assets/Scripts/DialogueChanging.cs
@@ -33,7 +33,9 @@ public class DialogueChanging : MonoBehaviour
 
     private bool isEnglish = true;
 
+    public string currentNodeColor;
 
+    public bool callChangTextOnLate;
 
     // Start is called before the first frame update
     void Start()
@@ -42,7 +44,7 @@ public class DialogueChanging : MonoBehaviour
         UI = DialogueRunner.GetComponent<DialogueUI>();
         isEnglish = GameObject.Find("Main Camera").GetComponent<SceneChanger>().isEnglish;
 
-        Debug.Log("HOw could it be" + isEnglish);
+        //Debug.Log("HOw could it be" + isEnglish);
 
         if (isEnglish)
         {
@@ -57,67 +59,84 @@ public class DialogueChanging : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+         
         if (Input.GetKeyDown("space"))
         {
             UI.MarkLineComplete();
+           
+        }
+        if(callChangTextOnLate){
+            callChangTextOnLate = false;
+            changText();
         }
     }
 
+
     public void startText()
     {
-        StartCoroutine("TextGoing");
+        callChangTextOnLate = true;
+        //StartCoroutine("TextGoing");
+
     }
 
     public IEnumerator TextGoing()
     {
+
         yield return new WaitForSeconds(2f);
+        //UI.MarkLineComplete();
+        //callChangTextOnLate = true;
         
-        changText();
-        UI.MarkLineComplete();
+    }
+
+    public void UpdateNodeColor(){
+        currentNodeColor = runner.variableStorage.GetValue("$bubble").AsString;
     }
 
     public void changText()
     {
         Debug.Log(runner.variableStorage.GetValue("$bubble").AsString);
+
         switch (runner.variableStorage.GetValue("$bubble").AsString)
         {
             case "PINK":
                 curText.transform.localPosition = new Vector3(600f, 325f, 0);
-                RedText.GetComponent<Image>().overrideSprite = OrangeImage;
-
+                RedText.GetComponent<Image>().overrideSprite = PinkImage;
 
                 break;
+
             case "BLUE":
                 curText.transform.localPosition = new Vector3(-575f, 322.5385f, 0);
                 RedText.GetComponent<Image>().overrideSprite = BlueImage;
 
 
                 break;
+
             case "GREEN":
                 curText.transform.localPosition = new Vector3(-575f, 322.5385f, 0);
                 RedText.GetComponent<Image>().overrideSprite = GreenImage;
 
 
                 break;
+
             case "ORANGE":
 
                 curText.transform.localPosition = new Vector3(600f, 325f, 0);
-                RedText.GetComponent<Image>().overrideSprite = PinkImage;
-
-
+                RedText.GetComponent<Image>().overrideSprite = OrangeImage;
 
                 break;
+
             case "RED":
                 curText.transform.localPosition = new Vector3(5.1f, -228.9f, 0);
 
                 RedText.GetComponent<Image>().overrideSprite = RedImage;
 
                 break;
+
             default:
                 break;
 
         }
-        Debug.Log(RedText.GetComponent<Image>().sprite.name);
+        //Debug.Log(RedText.GetComponent<Image>().sprite.name);
         
 
     }
